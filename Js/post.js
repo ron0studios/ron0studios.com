d4c3e0039febf841e75ff2ddfb960e11d9385659
rudrrayan <47331292+rudrrayan@users.noreply.github.com>
2019-11-28 17:34:55 +0000

    added
    added files

---
+++ Js/post.js
@@ -0,0 +1,21 @@
+
+function closeNav() {
+  document.documentElement.style.setProperty('--RIGHT_PANEL_WIDTH', '0px');
+  document.documentElement.style.setProperty('--ROT_AMOUNT', '0deg');
+}
+
+function openNav() {
+	document.documentElement.style.setProperty('--RIGHT_PANEL_WIDTH', '500px');
+	document.documentElement.style.setProperty('--ROT_AMOUNT', '90deg');
+}
+
+function buttonclick() {
+	if(getComputedStyle(document.documentElement).getPropertyValue('--RIGHT_PANEL_WIDTH') == '0px')
+	{
+		openNav();
+	}
+	else
+	{
+		closeNav();
+	}
+}
